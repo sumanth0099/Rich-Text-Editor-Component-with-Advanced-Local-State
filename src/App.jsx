bd679cd86e49bb310fc46c54bb02f11c14e4c944
sumanth0099 <tetalasumanthreddy@gmail.com>
2026-02-14 13:23:41 +0530

    Added App with configurations

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import RichTextEditor from './components/RichTextEditor/RichTextEditor'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Rich Text Editor Challenge</h1>
+      </header>
+      <main>
+        <RichTextEditor />
+      </main>
+    </div>
+  )
+}
+
+export default App
